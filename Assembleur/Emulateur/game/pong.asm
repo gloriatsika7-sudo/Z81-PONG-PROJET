ad9fc20c33840c184e8cd762c9c18ea30f23a923
gloriatsika7-sudo <gloriatsika7@gmail.com>
2026-01-29 23:31:59 +0100

    Add initial pong.asm file for Z81 project

---
+++ Assembleur/Emulateur/game/pong.asm
@@ -0,0 +1,10 @@
+;  Projet Pong - Z81
+ORG 2000h
+; ==========================================
+; PROJET Z81 PONG - ÉTUDIANT C
+; ==========================================
+
+ORG 2000h       ; Le programme commence à l'adresse 0x2000
+
+START:
+    HALT        ; Pour l'instant, on demande juste au processeur de s'arrêter
